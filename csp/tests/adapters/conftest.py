342dfae7889539bb2999162173442ed7b519b41f
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-02-03 15:58:28 -0500

    Setup CI for adapter with service dependencies
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- csp/tests/adapters/conftest.py
+++ csp/tests/adapters/conftest.py
@@ -5,13 +5,12 @@ from csp.adapters.kafka import KafkaAdapterManager
 
 @pytest.fixture(scope="module", autouse=True)
 def kafkabroker():
+    # Defined in ci/kafka/docker-compose.yml
     return "localhost:9092"
 
 
 @pytest.fixture(scope="module", autouse=True)
 def kafkaadapter(kafkabroker):
     group_id = "group.id123"
-    _kafkaadapter = KafkaAdapterManager(
-        broker=kafkabroker, group_id=group_id, rd_kafka_conf_options={"allow.auto.create.topics": "true"}
-    )
+    _kafkaadapter = KafkaAdapterManager(broker=kafkabroker, group_id=group_id)
     return _kafkaadapter

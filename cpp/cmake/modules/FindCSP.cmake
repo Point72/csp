70ce1eca4121c3e7e598431f8a81c2d9a4477a2a
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-03-13 19:55:51 -0400

    Cleanup references to ext, add math lib to cmake finder

--- cpp/cmake/modules/FindCSP.cmake
+++ cpp/cmake/modules/FindCSP.cmake
@@ -13,6 +13,8 @@
 # CSP_BASELIB_STATIC_LIBRARY
 # CSP_BASKETLIB_LIBRARY
 # CSP_BASKETLIB_STATIC_LIBRARY
+# CSP_MATH_LIBRARY
+# CSP_MATH_STATIC_LIBRARY
 # CSP_STATS_LIBRARY
 # CSP_STATS_STATIC_LIBRARY
 # CSP_NPSTATS_LIBRARY
@@ -27,8 +29,6 @@
 cmake_minimum_required(VERSION 3.7.2)
 
 find_package(Python ${CSP_PYTHON_VERSION} EXACT REQUIRED COMPONENTS Interpreter)
-
-set(ENV{PYTHONPATH} "${CMAKE_SOURCE_DIR}/ext:$ENV{PYTHONPATH}")
 
 # Find out the base path
 execute_process(
@@ -71,6 +71,9 @@ find_library(CSP_BASELIB_STATIC_LIBRARY NAMES libbaselibimpl_static.a PATHS "${_
 
 find_library(CSP_BASKETLIB_LIBRARY NAMES _cspbasketlibimpl.so PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)
 find_library(CSP_BASKETLIB_STATIC_LIBRARY NAMES libbasketlibimpl_static.a PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)
+
+find_library(CSP_MATH_LIBRARY NAMES _cspmathimpl.so PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)
+find_library(CSP_MATH_STATIC_LIBRARY NAMES libmathimpl_static.a PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)
 
 find_library(CSP_STATS_LIBRARY NAMES _cspstatsimpl.so PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)
 find_library(CSP_STATS_STATIC_LIBRARY NAMES libstatsimpl_static.a PATHS "${__csp_lib_path}" NO_DEFAULT_PATH)

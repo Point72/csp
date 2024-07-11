4eb82ae6615e0479aaff6ab5604dec71cf42a364
Adam Glustein <Adam.Glustein@Point72.com>
2024-07-11 11:25:58 -0400

    Clean up warnings from CSP code during build
    Signed-off-by: Adam Glustein <Adam.Glustein@Point72.com>

--- cpp/csp/engine/Struct.h
+++ cpp/csp/engine/Struct.h
@@ -756,11 +756,15 @@ private:
     void decref()
     {
 //Work around GCC12 bug mis-identifying this code as use-after-free
-//#pragma GCC diagnostic push
-//#pragma GCC diagnostic ignored "-Wuse-after-free"
+#ifdef __linux__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuse-after-free"
+#endif
         if( --hidden() -> refcount == 0 )
             delete this;
-//#pragma GCC diagnostic pop
+#ifdef __linux__
+#pragma GCC diagnostic pop
+#endif
     }
 
 

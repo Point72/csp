9189f78329e7791393a9d58589cc22bbaaf0cfb4
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-07-13 13:10:24 -0400

    NumPy 2.0 build support
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- csp/impl/types/numpy_type_util.py
+++ csp/impl/types/numpy_type_util.py
@@ -4,6 +4,6 @@ _TYPE_MAPPING = {numpy.dtype("float64"): float, numpy.dtype("int64"): int, numpy
 
 
 def map_numpy_dtype_to_python_type(numpy_dtype):
-    if numpy.issubdtype(numpy_dtype, numpy.unicode_):
+    if numpy.issubdtype(numpy_dtype, numpy.str_):
         return str
     return _TYPE_MAPPING.get(numpy_dtype, object)

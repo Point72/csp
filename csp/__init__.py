f10bc7a02a6ff74ce0b56ff76639fd96322c6353
Arham Chopra <arham.chopra@cubistsystematic.com>
2025-02-10 15:02:23 -0500

    Enable full stack trace if CSP_PRINT_EXCEPTION_FULL_STACK is set
    Signed-off-by: Arham Chopra <arham.chopra@cubistsystematic.com>

--- csp/__init__.py
+++ csp/__init__.py
@@ -40,3 +40,8 @@ def get_include_path():
 
 def get_lib_path():
     return os.path.join(os.path.dirname(__file__), "lib")
+
+
+if os.environ.get("CSP_PRINT_FULL_EXCEPTION_STACK", "0").lower() in ("1", "on", "true"):
+    # Print full stack trace if CSP_PRINT_FULL_EXCEPTION_STACK is set in the environment
+    set_print_full_exception_stack(True)

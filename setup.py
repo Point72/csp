7a24c8678a25deb0993b82faf00abb438daf6899
Adam Glustein <adam.glustein@point72.com>
2025-12-30 14:08:00 -0500

    Release v0.13.3
    Signed-off-by: Adam Glustein <adam.glustein@point72.com>

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ print(f"CMake Args: {cmake_args}")
 
 setup(
     name="csp",
-    version="0.13.2",
+    version="0.13.3",
     packages=["csp"],
     cmake_install_dir="csp",
     cmake_args=cmake_args,

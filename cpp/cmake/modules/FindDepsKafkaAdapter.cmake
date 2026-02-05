eb68a6e9d3f92876c811098cc34ffbd40ca63dd2
Krzysztof Milde <Krzysztof.Milde@Point72.com>
2026-02-05 11:06:51 -0500

    fix: update vcpkg to get avro-cpp 1.12.1 for fmt v12 compatibility
    - Update vcpkg submodule to f9d8eecab9 which has avro-cpp 1.12.1
    - Update vcpkg.json baseline to match
    - Update setup.py VCPKG_SHA to match
    - Add version check in FindDepsKafkaAdapter.cmake to gracefully skip
      Kafka adapter on Windows if avro-cpp < 1.12.1
    
    Signed-off-by: Krzysztof Milde <Krzysztof.Milde@Point72.com>

--- cpp/cmake/modules/FindDepsKafkaAdapter.cmake
+++ cpp/cmake/modules/FindDepsKafkaAdapter.cmake
@@ -1,40 +1,34 @@
 cmake_minimum_required(VERSION 3.7.2)
 
-# RdKafka is required for Kafka adapter
-find_package(RdKafka REQUIRED)
-
-# Workaround for vcpkg static library transitive dependencies not propagating properly
-# https://github.com/microsoft/vcpkg/issues/40320
-if(CSP_USE_VCPKG)
-  link_directories(${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
-endif()
-
-# Avro is optional - Kafka adapter works with JSON/raw bytes without it
-set(CSP_KAFKA_ENABLE_AVRO FALSE)
-
 if (CSP_USE_VCPKG)
-  find_package(unofficial-avro-cpp CONFIG)
+  find_package(RdKafka CONFIG REQUIRED)
+  find_package(unofficial-avro-cpp CONFIG REQUIRED)
 
-  if(unofficial-avro-cpp_FOUND)
-    # Check avro-cpp version on Windows - require >= 1.12.1 for fmt compatibility
-    if(WIN32 AND unofficial-avro-cpp_VERSION VERSION_LESS "1.12.1")
-      message(WARNING
-        "avro-cpp ${unofficial-avro-cpp_VERSION} has incompatible fmt::formatter on Windows. "
-        "Kafka Avro support will be disabled. Update vcpkg to get avro-cpp >= 1.12.1.")
-    else()
-      set(CSP_KAFKA_ENABLE_AVRO TRUE)
-      set(AVRO_LIBRARIES unofficial::avro-cpp::avrocpp CACHE INTERNAL "")
+  # Check avro-cpp version on Windows - require >= 1.12.1 for fmt v12 compatibility
+  # avro-cpp 1.12.0 has fmt::formatter with non-const format() which causes MSVC C2766
+  if(WIN32 AND unofficial-avro-cpp_VERSION VERSION_LESS "1.12.1")
+    message(WARNING
+      "avro-cpp ${unofficial-avro-cpp_VERSION} has incompatible fmt::formatter on Windows. "
+      "Avro support will be disabled. Update vcpkg to get avro-cpp >= 1.12.1.")
+    set(CSP_BUILD_AVRO OFF CACHE INTERNAL "")
+  else()
+    if(NOT WIN32)
+      # Bad, but a temporary workaround for
+      # https://github.com/microsoft/vcpkg/issues/40320
+      link_directories(${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
     endif()
+    set(CSP_AVRO_TARGET unofficial::avro-cpp::avrocpp CACHE INTERNAL "")
+    set(CSP_BUILD_AVRO ON CACHE INTERNAL "")
   endif()
-
+  set(DepsKafkaAdapter_FOUND TRUE)
 else()
+  find_package(RdKafka REQUIRED)
   find_package(Avro)
-
   if(Avro_FOUND)
-    set(CSP_KAFKA_ENABLE_AVRO TRUE)
-    set(AVRO_LIBRARIES Avro::avrocpp CACHE INTERNAL "")
+    set(CSP_AVRO_TARGET Avro::avrocpp CACHE INTERNAL "")
+    set(CSP_BUILD_AVRO ON CACHE INTERNAL "")
+  else()
+    set(CSP_BUILD_AVRO OFF CACHE INTERNAL "")
   endif()
+  set(DepsKafkaAdapter_FOUND TRUE)
 endif()
-
-# Kafka adapter is available if RdKafka is found
-set(DepsKafkaAdapter_FOUND TRUE)

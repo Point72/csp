eba77c247f8984bbd7e648f66c1f0ef282a7470e
Andrew Sasmito <asasmito1920@gmail.com>
2026-08-03 19:45:05 -0400

    Run linter
    Signed-off-by: Andrew Sasmito <asasmito1920@gmail.com>

--- cpp/csp/core/Platform.h
+++ cpp/csp/core/Platform.h
@@ -1,16 +1,16 @@
 #ifndef _IN_CSP_CORE_PLATFORM_H
 #define _IN_CSP_CORE_PLATFORM_H
-#include <type_traits>
 #include <stdint.h>
 #include <time.h>
+#include <type_traits>
 
-//TODO move Likely.h defines into Platform.h
+// TODO move Likely.h defines into Platform.h
 #ifdef WIN32
 #define NOMINMAX
-#include <windows.h>
 #include <assert.h>
-#include <synchapi.h>
 #include <bit>
+#include <synchapi.h>
+#include <windows.h>
 
 #undef ERROR
 #undef GetMessage
@@ -29,80 +29,78 @@
 #define CSPIMPL_EXPORT __declspec(dllimport)
 #endif
 
-#define START_PACKED __pragma( pack(push, 1) )
-#define END_PACKED   __pragma( pack(pop))
+#define START_PACKED __pragma(pack(push, 1))
+#define END_PACKED __pragma(pack(pop))
 
-#define NO_INLINE    __declspec(noinline)
+#define NO_INLINE __declspec(noinline)
 
-inline tm * localtime_r( const time_t * timep, tm * result )
-{
-    tm * rv = localtime(timep);
-    if (rv)
-        *result = *rv;
+inline tm *localtime_r(const time_t *timep, tm *result) {
+  tm *rv = localtime(timep);
+  if (rv)
+    *result = *rv;
 
-    return result;
+  return result;
 }
 
 #define timegm _mkgmtime
 
-inline int nanosleep(const timespec* req, timespec* rem)
-{
-    assert(rem == nullptr);
-    int64_t millis = req->tv_sec * 1000 + req->tv_nsec * 1000000;
-    Sleep(millis);
+inline int nanosleep(const timespec *req, timespec *rem) {
+  assert(rem == nullptr);
+  int64_t millis = req->tv_sec * 1000 + req->tv_nsec * 1000000;
+  Sleep(millis);
+  return 0;
+}
+
+inline constexpr uint8_t clz(uint64_t n) {
+  if (std::is_constant_evaluated()) {
+    return std::countl_zero(n);
+  } else {
+    unsigned long index = 0;
+    if (_BitScanReverse(&index, n))
+      return 64 - index - 1;
     return 0;
+  }
 }
 
-inline constexpr uint8_t clz(uint64_t n)
-{
-    if (std::is_constant_evaluated()) {
-        return std::countl_zero(n);
-    } else {
-        unsigned long index = 0;
-        if (_BitScanReverse(&index, n))
-            return 64 - index - 1;
-        return 0;
-    }
+inline constexpr uint8_t clz(uint32_t n) {
+  if (std::is_constant_evaluated()) {
+    return std::countl_zero(n);
+  } else {
+    unsigned long index = 0;
+    if (_BitScanReverse(&index, n))
+      return 32 - index - 1;
+    return 0;
+  }
 }
 
-inline constexpr uint8_t clz(uint32_t n)
-{
-    if (std::is_constant_evaluated()) {
-        return std::countl_zero(n);
-    } else {
-        unsigned long index = 0;
-        if (_BitScanReverse(&index, n))
-            return 32 - index - 1;
-        return 0;
-    }
+inline constexpr uint8_t clz(uint16_t n) {
+  return clz(static_cast<uint32_t>(n)) - 16;
+}
+inline constexpr uint8_t clz(uint8_t n) {
+  return clz(static_cast<uint32_t>(n)) - 24;
 }
 
-inline constexpr uint8_t clz(uint16_t n) { return clz(static_cast<uint32_t>(n)) - 16; }
-inline constexpr uint8_t clz(uint8_t n)  { return clz(static_cast<uint32_t>(n)) - 24; }
-
-template<typename U, std::enable_if_t<std::is_unsigned<U>::value, bool> = true>
-inline uint8_t ffs(U n)
-{ 
-    if (std::is_constant_evaluated()) {
-        return std::countr_zero(n);
-    } else {
-        unsigned long index = 0;
-        if (_BitScanForward(&index, n))
-            return index + 1;
-        return 0;
-    }
+template <typename U, std::enable_if_t<std::is_unsigned<U>::value, bool> = true>
+inline uint8_t ffs(U n) {
+  if (std::is_constant_evaluated()) {
+    return std::countr_zero(n);
+  } else {
+    unsigned long index = 0;
+    if (_BitScanForward(&index, n))
+      return index + 1;
+    return 0;
+  }
 }
 
-inline uint8_t ffs(uint64_t n)
-{
-    if (std::is_constant_evaluated()) {
-        return std::countr_zero(n);
-    } else {
-        unsigned long index = 0;
-        if (_BitScanForward(&index, n))
-            return index + 1;
-        return 0;
-    }
+inline uint8_t ffs(uint64_t n) {
+  if (std::is_constant_evaluated()) {
+    return std::countr_zero(n);
+  } else {
+    unsigned long index = 0;
+    if (_BitScanForward(&index, n))
+      return index + 1;
+    return 0;
+  }
 }
 
 #else
@@ -110,25 +108,34 @@ inline uint8_t ffs(uint64_t n)
 #define CSPIMPL_EXPORT
 #define CSPTYPESIMPL_EXPORT
 
-#define DLL_LOCAL __attribute__ ((visibility ("hidden")))
+#define DLL_LOCAL __attribute__((visibility("hidden")))
 
 #define START_PACKED
 #define END_PACKED __attribute__((packed))
 
-#define NO_INLINE  __attribute__ ((noinline))
+#define NO_INLINE __attribute__((noinline))
 
 inline constexpr uint8_t clz(uint32_t n) { return __builtin_clz(n); }
 inline constexpr uint8_t clz(uint64_t n) { return __builtin_clzl(n); }
 
-// clz (count leading zeros) returns number of leading zeros before MSB (i.e. clz(00110..) = 2 )
-// __builtin_clz auto-promotes to 32-bits: need to subtract off extra leading zeros
-inline constexpr uint8_t clz(uint16_t n) { return clz(static_cast<uint32_t>(n)) - 16; }
-inline constexpr uint8_t clz(uint8_t n)  { return clz(static_cast<uint32_t>(n)) - 24; }
+// clz (count leading zeros) returns number of leading zeros before MSB (i.e.
+// clz(00110..) = 2 )
+// __builtin_clz auto-promotes to 32-bits: need to subtract off extra leading
+// zeros
+inline constexpr uint8_t clz(uint16_t n) {
+  return clz(static_cast<uint32_t>(n)) - 16;
+}
+inline constexpr uint8_t clz(uint8_t n) {
+  return clz(static_cast<uint32_t>(n)) - 24;
+}
 
-// ffs (find first set) returns offset of first set bit (i.e. ffs(..0110) = 2 ), with ffs(0) = 0
-template<typename U, std::enable_if_t<std::is_unsigned<U>::value, bool> = true>
-inline constexpr uint8_t ffs( U n )        { return __builtin_ffs(n); }
-inline constexpr uint8_t ffs( uint64_t n ) { return __builtin_ffsl(n); }
+// ffs (find first set) returns offset of first set bit (i.e. ffs(..0110) = 2 ),
+// with ffs(0) = 0
+template <typename U, std::enable_if_t<std::is_unsigned<U>::value, bool> = true>
+inline constexpr uint8_t ffs(U n) {
+  return __builtin_ffs(n);
+}
+inline constexpr uint8_t ffs(uint64_t n) { return __builtin_ffsl(n); }
 
 #endif
 
